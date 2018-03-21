8f32dc7e1cbe7ff6e8761ad7dc8e69b411430f7b
anandgokul <anandgokul@arista.com>
2018-03-21 14:21:20 +0530

    Including even the version0 code folder

---
+++ TopologyGenerator-Initial/DRAFT1-v0-LldpNeighborListGenerator.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+
+#!/usr/bin/python
+
+import pexpect
+
+print "\t\t\t\t\tNeighbor Details of DUTS in testbed \n"
+
+cmd = "show lldp neighbor"
+
+dut = [ "fm367", "lf218", "ck338"]
+
+for i in xrange(0,len(dut)):
+   print dut[i]+ ":"
+   print ""
+   child = pexpect.spawn("ssh admin@"+dut[i],timeout=120)
+   child.expect(">")
+   child.sendline("enable")
+   child.expect("#")
+   child.sendline(cmd)
+   child.expect("#")
+   print(child.before)
+   print ""
+   print"-----------------------------------------------------------"
+   child.close()
+
+
