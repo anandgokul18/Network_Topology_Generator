f1d1334b8ab5e6caed96b7ac5188773678a662d1
anandgokul <anandgokul@arista.com>
2018-03-21 15:38:46 +0530

    Switching to swat mode

--- version5.py
+++ version5.py
@@ -48,75 +48,75 @@ def fileDutList(username,fileloc):
 		print "\n[ERROR]: File does not exist in "+fileloc+" . Please ensure correct file location to proceed \n"
 		sys.exit(1)
 
-def userDutList(username,poolname):
-	print "\n > Neighbor Details of DUTS in Art list output for user '"+username+ "':"
-
-	usernamelogin='anandgokul'
-	server='us128'
-	password='anandgokul123'
-	#*************************************************************************************
-	#The below code will use directly run Art list command and get output (NO SSH)
-
-	#The below try-except block will cover exception when user-server is unreachable
-	try:
-		child = pexpect.spawn("ssh "+ usernamelogin+ "@"+server,timeout=30)
-		child.expect("password:")
-		child.sendline(password)
-
-		cmd1= "Art list --pool="+poolname+" | grep "+ username
-		child.expect(">")
-		child.sendline(cmd1)
-
-		#Saving the output to duts1
-		child.expect(">")
-		duts1= (child.before)
-		#print duts1
-
-		#Splitting the output based on newline into list duts2
-		duts2=duts1.strip()
-		duts2= duts2.split("\n")
-		#print duts2
-
-		#Stripping the leading whitespaces in each element of above list. After that saving only the dut name in another list
-		duts3=[]
-		for i in xrange(0, len(duts2)):
-		   duts2[i]=duts2[i].strip()
-		   if i!=0:
-		      duts3.append((duts2[i].split(" "))[0])
-		#print duts3
-
-		#We got only the dut names as list in dut3. But, there is another trash entry at end which needs to be removed. 
-		dutslist=duts3[:-1]
-
-		print "\t * The DUTs owned by " + username +" are:  " + str(dutslist)
-
-		return dutslist
-
-	except Exception as e:
-		print "\n \t [ERROR] There was some issue with reaching the user servers. Please fix reachability to Arista Network \n"
-		print "* Script Complete!"
-		sys.exit()
-
-# #The below function uses SWAT library to find the list of DUTs owned by user
 # def userDutList(username,poolname):
+# 	print "\n > Neighbor Details of DUTS in Art list output for user '"+username+ "':"
+
+# 	usernamelogin='anandgokul'
+# 	server='us128'
+# 	password='anandgokul123'
+# 	#*************************************************************************************
+# 	#The below code will use directly run Art list command and get output (NO SSH)
+
+# 	#The below try-except block will cover exception when user-server is unreachable
+# 	try:
+# 		child = pexpect.spawn("ssh "+ usernamelogin+ "@"+server,timeout=30)
+# 		child.expect("password:")
+# 		child.sendline(password)
+
+# 		cmd1= "Art list --pool="+poolname+" | grep "+ username
+# 		child.expect(">")
+# 		child.sendline(cmd1)
+
+# 		#Saving the output to duts1
+# 		child.expect(">")
+# 		duts1= (child.before)
+# 		#print duts1
+
+# 		#Splitting the output based on newline into list duts2
+# 		duts2=duts1.strip()
+# 		duts2= duts2.split("\n")
+# 		#print duts2
+
+# 		#Stripping the leading whitespaces in each element of above list. After that saving only the dut name in another list
+# 		duts3=[]
+# 		for i in xrange(0, len(duts2)):
+# 		   duts2[i]=duts2[i].strip()
+# 		   if i!=0:
+# 		      duts3.append((duts2[i].split(" "))[0])
+# 		#print duts3
+
+# 		#We got only the dut names as list in dut3. But, there is another trash entry at end which needs to be removed. 
+# 		dutslist=duts3[:-1]
+
+# 		print "\t * The DUTs owned by " + username +" are:  " + str(dutslist)
+
+# 		return dutslist
+
+# 	except Exception as e:
+# 		print "\n \t [ERROR] There was some issue with reaching the user servers. Please fix reachability to Arista Network \n"
+# 		print "* Script Complete!"
+# 		sys.exit()
+
+#The below function uses SWAT library to find the list of DUTs owned by user
+def userDutList(username,poolname):
 	
-# 	alldevices=findDuts(pool=poolname, all=True)
-# 	devices=alldevices.items()
+	alldevices=findDuts(pool=poolname, all=True)
+	devices=alldevices.items()
 
-# 	dictOfDevicesbyuser=[]
-# 	listofDevicesbyuser=[]
+	dictOfDevicesbyuser=[]
+	listofDevicesbyuser=[]
 
-# 	for i,data in enumerate(devices):
-# 		if data[1]['owner']==username:
-# 			dictOfDevicesbyuser.append(data)
-# 			listofDevicesbyuser.append(data[0])
+	for i,data in enumerate(devices):
+		if data[1]['owner']==username:
+			dictOfDevicesbyuser.append(data)
+			listofDevicesbyuser.append(data[0])
 
-# 	#print dictOfDevicesbyuser
-# 	#print listofDevicesbyuser
+	#print dictOfDevicesbyuser
+	#print listofDevicesbyuser
 
-# 	print "\n > The DUTs owned by " + username +" are:  \n\t *  " + str(listofDevicesbyuser)
+	print "\n > The DUTs owned by " + username +" are:  \n\t *  " + str(listofDevicesbyuser)
 
-# 	return listofDevicesbyuser
+	return listofDevicesbyuser
 
 def excludeDutsFromList(finalListOfDuts,excluded):
 	#Removing the matches using intersections
