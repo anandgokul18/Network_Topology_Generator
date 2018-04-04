2f0fda701175f541847d957c41df228fc9c5a848
anandgokul <anandgokul@arista.com>
2018-04-04 21:00:01 +0530

    Removing virtualEnv scripts

--- VirtualEnv_Script/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#Before running this, we need to 'chmod +x script.sh' to grant write permissions to the script and then run './script.sh'
-
-python virtualenv_creator.py
-source "./.venv/bin/activate"
-# Virtualenv created now!
-python version6.py
